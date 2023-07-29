8b4f0735969c8a25d1d63ceba4d94a2cfdb9f7cb
aleff <aleff_diniz@hotmail.com>
2023-07-28 22:05:48 -0300

    Calculadora: component,service,module

---
+++ src/app/calculadora/components/calculadora.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+import { CalculadoraService } from '../service';
+@Component({
+  selector: 'app-calculadora',
+  templateUrl: './calculadora.component.html',
+  styleUrls: ['./calculadora.component.css']
+})
+export class CalculadoraComponent implements OnInit {
+
+  constructor(private calculadoraSevice:CalculadoraService) { }
+
+  ngOnInit() {
+  }
+
+}
